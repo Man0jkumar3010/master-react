d5ec798df03203b0b233f15a4f1c618cde6e66a4
Manoj Kumar <manojkumar.fsd3@gmail.com>
2026-05-28 20:05:41 +0530

    React Inception

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("1% Better Everyday")
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading 1"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Heading 2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2]
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+
+//async defer
+root.render(container);
