c4fae342d8a39038d52ab80abc9e0c576fe99b36
Manoj Kumar <manojkumar.fsd3@gmail.com>
2026-05-29 00:46:35 +0530

    Created a own react application using the parcel bundler

---
+++ README.md
@@ -0,0 +1,17 @@
+,,,
+
+# 1% Better Everyday 🚀
+,,,
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching Algorithm - Written in C++
+- Caching - Faster Build time
+- Image Optimazation
+- Bundling
+- Compressing file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - supports older browsers
--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+const parent = React.createElement("div", { id: "parent",key:1 }, [
+  React.createElement("div", { id: "child",key:2}, [
+    React.createElement("h1", {key:3}, "1% Better Everyday ❤️"),
+    React.createElement("h2", {key:4}, "I'm an h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
+
 ]);
 
 //JSX
